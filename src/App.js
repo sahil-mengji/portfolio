1b38fe18c8230568d76381168d2fcdd59da52945
sahil-mengji <mengjisahil@gmail.com>
2024-09-29 18:27:35 +0530

    rebuilt in react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import Work from "./pages/work/Work";
-import About from "./pages/about/About";
-import Contact from "./pages/contact/Contact";
+import Card from "./components/Main";
+import Detail from "./components/Detail";
 function App() {
 	return (
-		<div className="w-full h-[100vh] bg-[#090c10] text-white overflow-y-scroll perspective-[10px]">
-			<Navbar />
+		<div className="w-full  bg-black  h-[100vh] flex justify-center items-center flex-col">
 			<Routes>
-				<Route index path="/portfolio" element={<Home />} />
-				<Route path="#" element={<Home />} />
-				<Route path="/" element={<Home />} />
-				<Route path="/work" element={<Work />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/contact" element={<Contact />} />
+				<Route path="/" element={<Card />} />
+				<Route path="/detail/:id" element={<Detail />} />
 			</Routes>
 		</div>
 	);
